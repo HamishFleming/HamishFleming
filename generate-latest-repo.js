fb22732e820342b2941ad4915a4ab0110a63d528
Hamish <fleming_hamish@yahoo.com>
2025-11-06 07:48:02 +1100

    feat: Ensure GITHUB_TOKEN is set and fetch private repos

--- generate-latest-repo.js
+++ generate-latest-repo.js
@@ -3,11 +3,21 @@ import fs from "fs";
 
 const username = "HamishFleming";
 const readmePath = "./README.md";
+const token = process.env.GITHUB_TOKEN; // Must be set in your environment
 
 async function main() {
-    // Fetch latest updated repo
-    const res = await axios.get(`https://api.github.com/users/${username}/repos`, {
-        params: { sort: "updated", per_page: 1 },
+    if (!token) throw new Error("GITHUB_TOKEN not set");
+
+    // Fetch all repos (including private) accessible to the user
+    const res = await axios.get("https://api.github.com/user/repos", {
+        headers: {
+            Authorization: `token ${token}`,
+        },
+        params: {
+            sort: "updated",
+            direction: "desc",
+            per_page: 1,
+        },
     });
 
     const latestRepo = res.data[0].name;
@@ -17,10 +27,7 @@ async function main() {
     let readme = fs.readFileSync(readmePath, "utf-8");
 
     // Replace the current repo in badge URL
-    readme = readme.replace(
-        /(repo=)[^&]+/,
-        `$1${latestRepo}`
-    );
+    readme = readme.replace(/(repo=)[^&]+/, `$1${latestRepo}`);
 
     fs.writeFileSync(readmePath, readme);
     console.log(`Updated latest repo to: ${latestRepo}`);
